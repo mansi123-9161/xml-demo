370ce7557413bd49aa1fea2ed6dee988d3dd324c
mansi <mansi.chourasia@cgi.com>
2019-07-27 17:22:17 +0530

    updated task 5 with custom method

---
+++ src/main/java/com/stackroute/BeanLifeCycleDemo.java
@@ -0,0 +1,24 @@
+package com.stackroute;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class BeanLifeCycleDemo implements InitializingBean, DisposableBean {
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("initializing the bean life cycle");
+    }
+// this method does not print in first part of question
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroying bean");
+    }
+    //customDistroy method did not execue here after 2nd phase of question
+    public void customInit(){
+        System.out.println("Custom Bean Initialization method ... ");
+    }
+    public void customDestroy(){
+        System.out.println("Custom Bean Destroy method ... ");
+    }
+
+}
